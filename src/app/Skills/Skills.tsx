3606b4bee7118a8f07439dc3fc7ccf7ff55f611c
abdallah96 <gueye.amadou1996@gmail.com>
2025-03-24 00:12:11 +0100

    remove unused animation

--- src/app/Skills/Skills.tsx
+++ src/app/Skills/Skills.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import Lottie, { LottieOptions } from 'lottie-react';
+import Lottie from 'lottie-react';
 
 const Skills = () => {
   const [animationData, setAnimationData] = useState<any>(null);
